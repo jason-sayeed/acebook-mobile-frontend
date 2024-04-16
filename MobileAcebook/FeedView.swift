f643ba7e02d96363593591b6e476cf8743b9bf40
Danny Castilla <dancastiv@gmail.com>
2024-04-16 16:40:22 +0100

    feed view has button leading to new post view

--- MobileAcebook/FeedView.swift
+++ MobileAcebook/FeedView.swift
@@ -11,30 +11,33 @@ struct FeedView: View {
 
     var fakeFeed = ["Post 1", "Post 2", "Post 3", "A very long post, to see how it behaves. Once upon a midnight dreary while I pondered weak and weary...", "Another post", "If you're a bird be an early bird to catch a worm for your breakfast plate. If you're a bird be an early bird.\nBut if you're a worm, sleep late.", "And another post, mostly because I just want the stupid thing to scroll down goddammit. Someday I'll get there I'm sure."]
     var body: some View {
-        ScrollView(.vertical) {
-            VStack{
-                Image("makers-logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 90, height: 90)
-                    .accessibilityIdentifier("makers-logo")
-                Button("What's on your mind?") {
-                    //redirect to post view
+        NavigationView{
+            ScrollView(.vertical) {
+                VStack{
+                    Image("makers-logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 90, height: 90)
+                        .accessibilityIdentifier("makers-logo")
+                    NavigationLink(destination: NewPostView()) {
+                        Text("What's on your mind?")
+                    }
+                    .padding()
+                    List {
+                        
+                    }
+                    ForEach(fakeFeed, id: \.self) {item in
+                        Text("User goes here").fontWeight(.semibold)
+                            .multilineTextAlignment(.trailing)
+                        Text(item)
+                            .padding(10)
+                        Divider()
+                    }
+                    Spacer()
                 }
-                .padding()
-                List {
-                    
-                }
-                ForEach(fakeFeed, id: \.self) {item in
-                    Text("User goes here").fontWeight(.semibold)
-                        .multilineTextAlignment(.trailing)
-                    Text(item)
-                        .padding(10)
-                    Divider()
-                }
-                Spacer()
             }
         }
+        
         
     }
 }
